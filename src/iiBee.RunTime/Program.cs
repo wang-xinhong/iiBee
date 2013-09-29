eb8177958f2dde614724a970d9e70f3cc9844f40
AbraxasCSharp <armin.kogler@hotmail.com>
2013-09-29 20:11:46 +0200

    Added AutoStart Entry and AutoLogon Entry if not in Guest Mode Fixes #6

--- src/iiBee.RunTime/Program.cs
+++ src/iiBee.RunTime/Program.cs
@@ -1,9 +1,11 @@
 ﻿using iiBee.RunTime.WorkflowHandling;
+using Microsoft.Win32;
 using NLog;
 using System;
 using System.Configuration;
 using System.Diagnostics;
 using System.IO;
+using System.Reflection;
 
 namespace iiBee.RunTime
 {
@@ -16,6 +18,14 @@ namespace iiBee.RunTime
         static void Main(string[] args)
         {
             StartParameters startParams = new StartParameters(args);
+
+            if (!InGuestMode())
+            {
+                //Remove AutoLogon after Start, if it exists
+                DisableAutoLogon();
+                //Remove AutoStart entry after Start, if it exists
+                DisableAutoStart();
+            }
 
             WorkflowRunner wfRunner = null;
             if (storage.WorkflowIsStored())
@@ -64,11 +74,56 @@ namespace iiBee.RunTime
                 return;
             }
 
-            //TODO: Add logic for #6 here
+            EnableAutoLogon();
+            EnableAutoStart();
 
             string rebootArgs = "-r -f -t 10";
             log.Debug("Starting reboot with args[" + rebootArgs + "]");
             Process.Start("shutdown", rebootArgs);
+        }
+
+        /// <summary>
+        /// Enable AutoLogon with default user
+        /// </summary>
+        private static void EnableAutoLogon()
+        {
+            RegistryKey myKey = Registry.LocalMachine.CreateSubKey("SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\winlogon");
+            myKey.SetValue("AutoAdminLogon", 1, RegistryValueKind.String);
+            myKey.SetValue("DefaultUserName", ConfigurationManager.AppSettings["DefaultUserName"], RegistryValueKind.String);
+            myKey.SetValue("DefaultPassword", ConfigurationManager.AppSettings["DefaultPassword"], RegistryValueKind.String);
+            myKey.SetValue("DefaultDomainName", ConfigurationManager.AppSettings["DefaultDomainName"], RegistryValueKind.String);
+            myKey.Close();
+        }
+
+        /// <summary>
+        /// Disable AutoLogon and remove default user from registry
+        /// </summary>
+        private static void DisableAutoLogon()
+        {
+            RegistryKey myKey = Registry.LocalMachine.CreateSubKey("SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\winlogon");
+            myKey.SetValue("AutoAdminLogon", 0, RegistryValueKind.String);
+            myKey.DeleteValue("DefaultUserName");
+            myKey.DeleteValue("DefaultPassword");
+            myKey.DeleteValue("DefaultDomainName");
+            myKey.Close();
+        }
+
+        /// <summary>
+        /// Set an AutoStart Entry in Registry for this Application
+        /// </summary>
+        private static void EnableAutoStart()
+        {
+            RegistryKey myKey = Registry.CurrentUser.CreateSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run\\wBee");
+            myKey.SetValue("wBee", Assembly.GetExecutingAssembly().GetName().CodeBase, RegistryValueKind.String);
+            myKey.Close();
+        }
+
+        /// <summary>
+        /// Remove the AutoStart Entry for this Application
+        /// </summary>
+        private static void DisableAutoStart()
+        {
+            Registry.CurrentUser.DeleteSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run\\wBee");
         }
 
         /// <summary>
